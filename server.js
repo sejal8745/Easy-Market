0660dc7ec107b0c0ae6bd7a35d3aa6c91a498ef4
Sejal Goyal <sejal@Sejals-MacBook-Air.local>
2023-11-03 12:40:19 +0530

    simpy run the app over the port

---
+++ server.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+
+// const colors = require('colors')
+import express from 'express';
+import colors from 'colors';
+
+//rest object: to use api
+const app = express();
+
+//rest api
+app.get('/', (req,res) => {
+    res.send({
+        message: 'Welcome to E-commerce app'
+    })
+})
+
+//PORT
+const PORT = 8080
+
+//run the app
+app.listen(PORT, ()=>{
+    console.log(`Server is running ${PORT}`.bgGreen.white);
+})
