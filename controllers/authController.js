fa6c2365b517bb1262a570055c2977867c959a5d
Sejal Goyal <sejal@Sejals-MacBook-Air.local>
2023-11-04 07:06:16 +0530

    Login Route Created using JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashpassword } from "../utils/authUtil.js";
-
+import { comparePassword, hashpassword } from "../utils/authUtil.js";
+import JWT from "jsonwebtoken";
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -52,6 +52,62 @@ export const registerController = async (req, res) => {
       success: false,
       message: "Error in registration",
       erorr,
+    });
+  }
+};
+
+//login
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //validation
+
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    ///check user
+    const user = await userModel.findOne({ email: email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "email is not registered",
+      });
+    }
+
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    //token
+
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "14d",
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "login successfully",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in login",
+      error,
     });
   }
 };
