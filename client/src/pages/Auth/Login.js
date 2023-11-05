f62222dd6e5dc461bba2d2839de23014721a916d
sejal8745 <sejalgoyal16@gmail.com>
2023-11-05 12:22:59 +0530

    login page created

---
+++ client/src/pages/Auth/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import Layout from "../../components/layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Authstyle.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/login", {
+        email,
+        password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <Layout title="Login">
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <h4 className="title"> Login Here</h4>
+
+          <div className="mb-3">
+            <input
+              required
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter your Email Address"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              required
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter a strong Password"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default Login;
