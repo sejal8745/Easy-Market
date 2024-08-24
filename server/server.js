d00e670f9c6f1a32302eb615ecff35fa4053c442
sejal8745 <sejalgoyal16@gmail.com>
2024-08-24 11:46:42 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -47,10 +47,10 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// //rest api
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 //rest api
 app.get("/", (req, res) => {
